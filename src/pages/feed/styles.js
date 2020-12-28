429e5e2436164631ca0950b065d4044b071056d3
Ramon Uchoa <ramonuchoa386@gmail.com>
2020-12-28 11:58:52 -0300

    removendo css antigo

--- src/pages/feed/styles.js
+++ src/pages/feed/styles.js
@@ -1,11 +1,5 @@
 import { StyleSheet } from 'react-native';
 export default styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
   titulo: {
     fontSize: 20,
   },
