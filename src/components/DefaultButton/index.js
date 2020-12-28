6716e26880fcd47e35b0cf82d7c37e5f7b50b83b
Ramon Uchoa <ramonuchoa386@gmail.com>
2020-12-27 21:39:52 -0300

    migrando estilos do botao para styled component

--- src/components/DefaultButton/index.js
+++ src/components/DefaultButton/index.js
@@ -1,55 +1,16 @@
 import React from 'react';
-import { StyleSheet, TouchableOpacity, Text, View } from 'react-native';
+import { Container, Button, Content } from './style.js';
 
 function DefaultButton(props) {
-  console.log(props)
-  return (
-    <View style={[styles.container, props.marginBottom == 'true' ? styles.marginBottom : '' ]}>
-      <TouchableOpacity
-        style={props.color == 'purple' ? styles.btnPurple : styles.btnWhite}
-        onPress={props.onPress}>
-        <Text style={props.color == 'purple' ? styles.txtPurple : styles.txtWhite} >{props.children}</Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
+	return (
+		<Container>
+			<Button colored={ props.colored } onPress={ props.onPress }>
+				<Content colored={ props.colored }>
+					{ props.children }
+				</Content>
+			</Button>
+		</Container>
+	);
+};
 
 export default DefaultButton;
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  btnPurple: {
-    flex: 1,
-    height: 50,
-    borderRadius: 50,
-    alignItems: 'center',
-    justifyContent: "center",
-    backgroundColor: "#4898DD",
-  },
-  btnWhite:{
-    flex: 1,
-    borderRadius: 50,
-    height: 50,
-    alignItems: 'center',
-    justifyContent: "center",
-    backgroundColor: "#fff",
-  },
-  txtPurple:{
-    color: "#fff",
-    fontSize: 20,
-    fontWeight: "bold"
-  },
-  txtWhite:{
-    color: "#4898DD",
-    fontSize: 20,
-    fontWeight: "bold"
-  },
-  marginBottom: {
-    marginBottom: 20,
-  },
-  
-})
