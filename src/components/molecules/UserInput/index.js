a0d755617e8b90f6719bd2de0e6fd842d187c626
Ramon Uchoa <ramonuchoa386@gmail.com>
2020-12-30 08:05:31 -0300

    movendo componente de input para folder molecules

--- src/components/molecules/UserInput/index.js
+++ src/components/molecules/UserInput/index.js
@@ -4,15 +4,13 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 import { InputIcon, InputRow, InputField } from './style.js';
 
 function UserInput(props) {
-    console.log(props);
-    
     return (
         <InputRow>
-            <InputIcon name={props.iconSet[0]} size={props.iconSet[1]} color={props.iconSet[2]} /> 
-            <InputField
-                value={props.value}  
+            <InputIcon name={props.iconSet[0]} size={props.iconSet[1]} color={props.iconSet[2]} />
+            <InputField value={props.value}  
                 placeholder={props.placeholder}
-                onChangeText={props.onChangeText} />
+                onChangeText={props.onChangeText}
+                secureTextEntry={props.secureTextEntry} />
             {props.children}
         </InputRow>
     );
