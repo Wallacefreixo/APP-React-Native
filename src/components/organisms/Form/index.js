900891fea891a9d8dbcfa4221e14875f445e9a69
Ramon Uchoa <ramonuchoa386@gmail.com>
2020-12-31 00:17:28 -0300

    criando componente de Form

---
+++ src/components/organisms/Form/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { FlexWrapper } from '../../PageWrapper/style';
+import { UserInput, DefaultButton } from '../../molecules/';
+import { Error, Title } from '../../atoms/Content/style';
+
+function Form(props) {
+    const fields = [];
+
+	for( let i = 0; i < props.fields.length; i++ ) {
+		fields.push(
+            <UserInput key={i}
+                value={props.fields[i][0]}
+                iconSet={props.fields[i][1]}
+                placeholder={props.fields[i][2]}
+                onChangeText={props.fields[i][3]}
+                secureTextEntry={props.fields[i][4]}>
+
+                { props.fields[i][5] }
+
+            </UserInput>
+		)
+    }
+    
+    return (
+        <FlexWrapper marginTop='40px'>
+            <Title>{props.formTitle}</Title>
+
+            { fields }
+
+            <Error description>{props.errorMessage}</Error> 
+            <DefaultButton colored onPress={props.submit}>{props.formTitle}</DefaultButton>
+            { props.children }
+        </FlexWrapper>
+    );
+};
+
+export default Form;
