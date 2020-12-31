661e57eedaa2c03303d1b9bda28585c57a8687d2
Ramon Uchoa <ramonuchoa386@gmail.com>
2020-12-31 02:32:21 -0300

    movendo estilos de botoes para o componente botao

--- src/components/molecules/DefaultButton/style.js
+++ src/components/molecules/DefaultButton/style.js
@@ -9,3 +9,26 @@ export const Button = styled.TouchableOpacity`
 	justify-content: center;
 	background-color: ${ props => props.colored ? props.theme.color.lighter : '#fff' };
 `;
+
+export const ShowPass = styled.TouchableOpacity`
+  position: absolute;
+  top: 18px;
+  right: 20px;
+`;
+
+export const SimpleIcon = styled.TouchableOpacity`
+  margin: 20px;
+`;
+
+export const SimpleText = styled.TouchableOpacity`
+  align-items: flex-end;
+  padding-top: 20px;
+  padding-right: 10px;
+`;
+
+export const FullBox = styled.TouchableOpacity`
+  background-color: ${props => props.theme.color.darken};
+  height: 60px;
+  align-items: center;
+  justify-content: center;
+`;
